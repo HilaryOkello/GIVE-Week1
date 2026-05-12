39f8b58cdd01bc9dde49917e441dff8c9366de8c
HilaryOkello <hilaryokello091@gmail.com>
2026-05-12 04:35:28 +0300

    add task 3

---
+++ task3/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let number = 17;
+
+    if number > 5 {
+        println!("greater than 5");
+    } else if number < 5 {
+        println!("less than 5");
+    } else {
+        println!("=5");
+    }
+}
